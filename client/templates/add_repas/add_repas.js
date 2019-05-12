9d55ac4e5ca75341a6496e186d41f10a10e1a3c4
Sinem <sinem_eylul_91@hotmail.com>
2019-05-12 15:48:32 +0200

    la page detail, ajouter repas form, routing pour la page detail ont change

--- client/templates/add_repas/add_repas.js
+++ client/templates/add_repas/add_repas.js
@@ -1,10 +1,16 @@
 //Pour récupérer le formulaire
 Template.add_repas.events({
 	"submit .add_repas": function(event){
-		var name = event.target.name.value;
-		var category = event.target.category.value;
-		var description = event.target.description.value;
-		var is_featured = event.target.is_featured.value;
+		event.preventDefault();
+		var nom = event.target.nom.value;
+        var portions = event.target.portions.value;
+        var prix = event.target.prix.value;
+        var ingredients = event.target.ingredients.value;
+        var lieu = event.target.lieu.value;
+        var type = event.target.type.value;
+        var description = event.target.description.value;
+        var adresse = event.target.adresse.value;
+        var estVeg = event.target.estVeg.value;
 
 		var file = $('#repasImage').get(0).files[0];
 
@@ -17,10 +23,15 @@ Template.add_repas.events({
 					console.log(repasImage)
 
 					Repas.insert({
-						name: name,
-						category: category,
-						description: description,
-						is_featured: is_featured,
+						nom: nom,
+    		            portions: portions,
+    		            prix: prix,
+    		            ingredients: ingredients,
+    		            lieu: lieu,
+    		            type: type,
+    		            description: description,
+    		            adresse: adresse,
+    		            estVeg: estVeg,
 						image: repasImage,
 						createdAt: new Date()
 					});
@@ -30,20 +41,25 @@ Template.add_repas.events({
 			var repasImage = '/img/noimage.png';
 
 			Repas.insert({
-				name: name,
-				category: category,
+				nom: nom,
+				portions: portions,
+				prix: prix,
+				ingredients: ingredients,
+				lieu: lieu,
+				type: type,
 				description: description,
-				is_featured: is_featured,
+				adresse: adresse,
+				estVeg: estVeg,
 				image: repasImage,
 				createdAt: new Date()
 			});
 		}
 
 		// Clear form
-	    event.target.name.value = "";
-	    event.target.category.value = "";
+	    event.target.nom.value = "";
+	    event.target.portions.value = "";
 	    event.target.description.value = "";
-	    event.target.is_featured.value = "";
+	    event.target.ingredients.value = "";
 
 	    FlashMessages.sendSuccess("Repas ajouté");
 	    Router.go('/');
