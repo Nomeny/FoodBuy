10b6bda021a3580cf332b921e681a1880a4a4cbe
Squidez <44018884+Squidez@users.noreply.github.com>
2019-05-23 14:11:57 +0200

    Correction architecture

--- client/templates/commentaires/commentaires.js
+++ client/templates/commentaires/commentaires.js
@@ -19,14 +19,3 @@ Template.commentaires.events({
         }
     }
 })
-
-//affiche l'utilisateur et l'heure de l'envoie du message
-Template.message.helpers({
-        username: function() {
-            return Meteor.users.findOne({_id: this.user});
-        },
-
-        tempsMS: function() {
-            return moment(this.tempsenvoie).format('LT');
-        }
-});
---
+++ client/templates/message/message.js
@@ -0,0 +1,11 @@
+
+//affiche l'utilisateur et l'heure de l'envoie du message
+Template.message.helpers({
+    username: function() {
+        return Meteor.users.findOne({_id: this.user});
+    },
+
+    tempsMS: function() {
+        return moment(this.tempsenvoie).format('LT');
+    }
+});
