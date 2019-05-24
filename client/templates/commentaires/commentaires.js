fbd97644fa8f12cff41f7f43f339e65632f9bc8c
Squidez <44018884+Squidez@users.noreply.github.com>
2019-05-24 11:09:15 +0200

    scrollbar pour les commentaires

--- client/templates/commentaires/commentaires.js
+++ client/templates/commentaires/commentaires.js
@@ -1,12 +1,8 @@
 Template.commentaires.helpers({
     commentaires: function() {
-        return Commentaires.find();
+        return Commentaires.find({},{$elemMatch: {roomId: this.currentRoomId}});
     }
 });
-
-// Template.commentaires.helpers({
-//     commentaires: function 
-// })
 
 //envoyer un commentraire en appuyant sur la touche "Enter"
 Template.commentaires.events({
@@ -14,7 +10,9 @@ Template.commentaires.events({
         if (e.keyCode == 13){
             let text = instance.find('textarea').value;
             instance.find('textarea').value = '';
-
+            
+            let currentRoomId = Repas.findOne({_id: this._id});
+            
             Commentaires.insert({
                 message: text,
                 tempsenvoie: new Date(),
