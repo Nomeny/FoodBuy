11a030a881c770f162a366c3957f14db8c51f25c
Squidez <44018884+Squidez@users.noreply.github.com>
2019-05-26 15:26:04 +0200

    ajout du package rocketchat

--- client/templates/commentaires/commentaires.js
+++ client/templates/commentaires/commentaires.js
@@ -10,14 +10,11 @@ Template.commentaires.events({
         if (e.keyCode == 13){
             let text = instance.find('textarea').value;
             instance.find('textarea').value = '';
-             
-            let currentRoomId = Repas.findOne({_id: this._id});
-            
+                         
             Commentaires.insert({
                 message: text,
                 tempsenvoie: new Date(),
                 username: Meteor.user().username,
-                roomId: currentRoomId
             });
         }
     }
