ce34b43c58db57894504fab9378d7bbdea853edb
Paul <44018884+Squidez@users.noreply.github.com>
2019-05-29 07:14:04 +0200

    Typo
    correnction typo

--- client/templates/layout.js
+++ client/templates/layout.js
@@ -1,5 +1,5 @@
 // mise en forme Texte description
-Template.registerHelper('fromeTexte', function(text, length){
+Template.registerHelper('formeTexte', function(text, length){
 	var newText = text.substring(0, length);
 	newText = newText.substr(0, Math.min(newText.length, newText.lastIndexOf(" "))) //ne pas couper le mot
 	return new Spacebars.SafeString(newText)
